a84ea549d9ce0acc699190eea55d60cc5d8d55ea
Patryk Ptasiński <github@ipepe.pl>
2025-06-05 16:23:59 +0200

    Implement basic HTML-to-text conversion

--- spec/crhtml2markdown_spec.cr
+++ spec/crhtml2markdown_spec.cr
@@ -1,10 +1,8 @@
 require "./spec_helper"
 
 describe Crhtml2markdown do
-  # TODO: Write tests
-
-  it "works" do
-    # Placeholder test ensuring the spec suite runs
-    true.should eq(true)
+  it "converts HTML to text" do
+    html = "<p>Hello <strong>world</strong></p>"
+    Crhtml2markdown.convert(html).should eq("Hello world")
   end
 end
