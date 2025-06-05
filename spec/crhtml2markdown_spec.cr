8ba1c991aede47233973c2483d4d9b879086ee9e
Patryk Ptasiński <patryk@ipepe.pl>
2025-06-05 15:55:29 +0200

    Initialize project with base structure, configuration, and license.

---
+++ spec/crhtml2markdown_spec.cr
@@ -0,0 +1,9 @@
+require "./spec_helper"
+
+describe Crhtml2markdown do
+  # TODO: Write tests
+
+  it "works" do
+    false.should eq(true)
+  end
+end
---
+++ src/crhtml2markdown.cr
@@ -0,0 +1,6 @@
+# TODO: Write documentation for `Crhtml2markdown`
+module Crhtml2markdown
+  VERSION = "0.1.0"
+
+  # TODO: Put your code here
+end
