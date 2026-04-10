83c6b8a3c7c77de4174c9a45ebf4359bb16949d1
Patryk Ptasinski <patryk@ipepe.pl>
2026-04-10 01:01:19 +0000

    Clean up test files

--- test_attrs.cr
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env crystal
-
-require "./src/crhtml2markdown"
-
-html = %(<p><acronym title='Has "quotes"'>ABC</acronym></p>)
-puts "Input HTML: #{html}"
-puts "Output markdown: #{Crhtml2markdown.convert(html).inspect}"
--- test_rt.cr
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env crystal
-
-require "./src/crhtml2markdown"
-
-html = %(<ruby>Base<rt title='Has "quotes"'>ruby text</rt></ruby>)
-puts "Input HTML: #{html}"
-puts "Output markdown:"
-puts Crhtml2markdown.convert(html)
